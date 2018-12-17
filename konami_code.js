1ca9356465284c997014fda90158a291897a25eb
lauren kernan <lauren.kernan17@gmail.com>
2018-12-17 19:22:40 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -9,10 +9,10 @@ let index = 0;
 function onKeyDownHandler(e) {
   const key = parseInt(e.detail || e.which);
  
-  if (key === alphabet[index]) {
+  if (key === code[index]) {
     index++;
  
-    if (index === alphabet.length) {
+    if (index === code.length) {
       alert("Hurray!");
  
       index = 0;
