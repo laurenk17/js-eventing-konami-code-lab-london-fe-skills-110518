6eab03f918afe394241c4996b4226810f6393346
lauren kernan <lauren.kernan17@gmail.com>
2018-12-17 19:13:43 +0000

    Automatically backed up by Learn

--- konami_code.js
+++ konami_code.js
@@ -4,7 +4,7 @@ function init() {
   // Write your JavaScript code inside the init() function
 const body = document.body
 let index = 0;
- body.addEventListener
+ body.addEventListener(keydown)
 // This is the function that would be invoked by the event listener.
 function onKeyDownHandler(e) {
   const key = parseInt(e.detail || e.which);
